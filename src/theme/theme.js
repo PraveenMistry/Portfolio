47b2df0d5ddde3a7fe7a76d87928fd2ddc6bc564
Praveen <praveensuthar108@gmail.com>
2022-09-11 13:42:38 +0530

    Update theme - profile pic

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -4,7 +4,7 @@ import {
     orangeThemegirl, pinkThemeboy, pinkThemegirl, purpleThemeboy, purpleThemegirl, redThemeboy, redThemegirl, yellowThemeboy,
     yellowThemegirl,eduBlack,eduBlue,eduGreen,eduOrange,eduPink,eduPurple,eduRed,eduYellow,expBlack,expBlue,expGreen,expOrange,
     expPink,expPurple,expRed,expYellow,contactsBlack,contactsBlue,contactsGreen,contactsOrange,contactsPink,contactsPurple,
-    contactsRed,contactsYellow
+    contactsRed,contactsYellow, profile
 } from './images'
 
 
@@ -111,6 +111,7 @@ export const blueThemeLight = {
     tertiary50: '#21212180',
     aboutimg1: blueThemeboy,
     aboutimg2: blueThemegirl,
+    profile: profile,
     eduimg: eduBlue,
     expimg: expBlue,
     contactsimg: contactsBlue
