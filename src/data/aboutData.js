9a80ab0a0a9868a5e7f3769b74ab50ed2944380f
Praveen <praveensuthar108@gmail.com>
2022-09-11 13:39:49 +0530

    update about images

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -2,5 +2,5 @@ export const aboutData = {
     title: "Who I am",
     description1: "I'm a Full Stack Developer (SDE3) based in Gurgaon India",
     description2: "During the day I work as a lead developer at a local agency and in the evening I work on freelance projects and utilize the time to built my own products. I spent my leisure hours reading articles and poetry. Right now I'm also trying a hand at machine learning and AI. I love to learn and explore new arenas.",
-    image: 2
+    image: 1
 }
