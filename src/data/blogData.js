d7710776bba76e8247e9b0f463a72f334b2bd35f
Praveen <praveensuthar108@gmail.com>
2022-09-11 13:20:07 +0530

    update blog data

--- src/data/blogData.js
+++ src/data/blogData.js
@@ -1,19 +1,19 @@
 export const blogData = [
     {
         id: 1,
-        title: 'Stack Implementation | Javscript',
-        description: 'Some common data structures and implement them in JavaScript. Stack, Queue, Linked List, Heap, Set, etc',
+        title: 'Stack | Javascript | Part-1',
+        description: 'Some common data structures and implement them in JavaScript. Stack, Queue, Linked List, Heap, Set, etc. Here is Stack',
         date: 'Sep 7, 2022',
         image: 'https://miro.medium.com/max/1400/1*8hMewp1C9JU4POLqT2xaoQ.jpeg',
         url: 'https://medium.com/@praveenmistry/stack-javascript-part-1-15f9e6ebecd5'
     },
     {
         id: 2,
-        title: 'VALID PARENTHESES',
-        description: 'This article talks about a problem famously known as Valid Parentheses. True to its name the problem deals with finding whether a given string has a valid set of parentheses. A parenthesis is said to be balanced if each left parenthesis has its respective right parenthesis to match its pair in a well-nested format.',
-        date: 'Sep 7, 2022',
-        image: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh-NiSGIwMH6cxCDIaxUnPafa4x-PX60zr6ec42aWELk-eMITWRztZHw31ksOxgBI_hA2MZSnRO9apsCf84axhOe8QB4c-Q-SllTAlHFnTP2U3LuVcQpkDOueBnU0OPXryvhXT6Bc04Mu4ckh6e2-nS1QNJDmUNAiD12ODmCIz-OFQnUEpaiXklxHjxWA/w568-h290/Screenshot%202022-09-08%20at%205.12.12%20PM.png',
-        url: 'https://sudomistry.blogspot.com/2022/09/20valid-parentheses-leet-code-stack.html'
+        title: 'Queue | Javascript | Part-2',
+        description: 'Some common data structures and implement them in JavaScript. Stack, Queue, Linked List, Heap, Set, etc. Here is Queue',
+        date: 'Sep 10, 2022',
+        image: 'https://miro.medium.com/max/1400/1*uRpXar4J4e3IV76X4K9ekA.jpeg',
+        url: 'https://medium.com/@praveenmistry/queue-javascript-part-2-5dab626110aa'
     },
 ]
 
