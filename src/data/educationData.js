02e4e4d3a9f4a1027a85faeaf1f4de4d6df1dd69
Praveen <praveensuthar108@gmail.com>
2022-09-20 22:56:53 +0530

    update blogs and education data

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,14 +1,13 @@
 export const educationData = [
     {
         id: 1,
-        institution: 'RAJASTHAN TECHNICAL UNIVERSITY, KOTA',
+        // institution: 'BACHELOR OF TECHNOLOGY',
         course: 'BACHELOR OF TECHNOLOGY',
         startYear: '2011',
         endYear: '2015'
     },
     {
         id: 2,
-        institution: 'GOVT. SR.SEC.SCHOOL, SIROHI',
         course: 'SENIOR SECONDARY',
         startYear: '2009',
         endYear: '2011'
